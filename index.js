41dd549d69abc3e05455134f5b06d7b871773ec4
pengshi2 <boogiesnoogawoot@gmail.com>
2022-11-02 10:11:57 -0400

    b

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-const domSelectors = {};
+// const domSelectors = {};
+// const nodeList = document.querySelectorAll("items");
+// for (let i = 0; i < nodeList.length; i++) {
+//   nodeList[i].style.backgroundColor = "red";
+// }
+
+const allPeople = document.querySelectorAll(".people");
+console.log(allPeople);
