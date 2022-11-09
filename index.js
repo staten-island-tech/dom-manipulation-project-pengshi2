5f2723e089b05cdf847a86b3c4d10f57fe5bb4a9
pengshi2 <boogiesnoogawoot@gmail.com>
2022-11-09 10:10:45 -0500

    d

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
-// const domSelectors = {
-//   title: document.getElementById("title"),
-//   artist: document.getElementById("artist"),
-//   url: document.getElementById("url"),
-//   display: document.getElementById("display"),
-// };
+const domSelectors = {
+  title: document.getElementById("title"),
+  artist: document.getElementById("artist"),
+  url: document.getElementById("url"),
+  display: document.getElementById("display"),
+  reset: document.getElementById("btn"),
+};
 const allPeople = document.querySelectorAll(".person");
 console.log(allPeople);
 
@@ -15,4 +16,8 @@ function myFunction() {
 
 function theFunction() {
   document.getElementById("myDiv").style.backgroundImage = "url('1999.jpg')";
+}
+
+function fun() {
+  document.getElementById("myForm").reset();
 }
