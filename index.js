fdf5784c57a4c0827c34c8c23343fc2f6c0c08c4
pengshi2 <boogiesnoogawoot@gmail.com>
2022-11-15 10:10:59 -0500

    b

--- index.js
+++ index.js
@@ -6,24 +6,10 @@ const DOMselectors = {
   display: document.getElementById("display"),
   reset: document.getElementById("btn"),
 };
-const allPeople = document.querySelectorAll(".person");
-console.log(allPeople);
-
-function myFunction() {
-  document.body.style.backgroundColor = "red";
-}
-
-function theFunction() {
-  document.getElementById("myDiv").style.backgroundImage = "url('1999.jpg')";
-}
 
 function fun() {
   document.getElementById("myForm").reset();
 }
-
-let title = document.theform.title.value;
-console.log(document.theform.title.value);
-
 let randomColor = "#" + Math.floor(Math.random() * 16777215).toString(16);
 
 console.log(randomColor);
@@ -31,5 +17,8 @@ console.log(randomColor);
 function color() {
   let element = document.getElementById("myDiv");
   element.style.backgroundColor = randomColor;
-  String.Repeat();
+}
+
+function fortnite() {
+  myDiv.style.backgroundColor = "white";
 }
