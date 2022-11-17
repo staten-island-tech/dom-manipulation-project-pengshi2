d418bbacaa690787df7e224087a705d9724b90a7
pengshi2 <boogiesnoogawoot@gmail.com>
2022-11-17 10:06:56 -0500

    b

--- index.js
+++ index.js
@@ -7,18 +7,26 @@ const DOMselectors = {
   reset: document.getElementById("reset"),
 };
 
-function fun() {
-  document.getElementById("myForm").reset();
-}
 let randomColor = "#" + Math.floor(Math.random() * 16777215).toString(16);
 
 console.log(randomColor);
+
+const button = document.getElementById("submit");
+button.addEventListener("click", () => {
+  {
+    let boxInput = document.getElementById("task").value;
+    let listItem = document.createElement("li");
+    let text = document.createTextNode(boxInput);
+    listItem.appendChild(text);
+  }
+  let list = document.getElementById("list");
+  list.appendChild(listItem);
+});
 
 function color() {
   let element = document.getElementById("myDiv");
   element.style.backgroundColor = randomColor;
 }
-
 document.getElementById("myDiv").onclick = color();
 
 const deleteButtons = document.querySelectorAll(".del");
